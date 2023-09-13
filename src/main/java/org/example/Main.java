850508bff579128d9492a1e021003fe9fc65b61a
Jana <jana.loewenzahn07@gmail.com>
2023-09-13 13:15:52 +0200

    server and client startpoint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.application.Game;
+import org.example.server.Server;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Server server = new Server(new Game());
     }
 }
--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,5 +1,7 @@
 package org.example.server;
 
+import org.example.application.Game;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,31 +10,48 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    static final int PORT = 5934;
+    private int PORT = 5934;
+    private ServerSocket serverSocket;
+    private final Game game;
 
-    public static void main(String args[]) throws IOException {
-        //System.out.println("Start server...");
-        ServerSocket serverSocket = new ServerSocket(PORT);
+    public Server(Game game) {
+        this.game = game;
+    }
+
+    private void start() throws IOException {
+        System.out.println("Server start");
+        serverSocket = new ServerSocket(this.PORT);
+        System.out.println("Server runs on Port" + PORT);
+
+    }
+
+    private void run() {
         while (true) {
-            Socket socket = serverSocket.accept();
-            new Thread(() ->{
-                try {
-                    BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintStream output = new PrintStream(socket.getOutputStream());
-                    while (true) {
-                        String line = input.readLine();
-                        if (line == null || line.equalsIgnoreCase("quit")) {
-                            socket.close();
-                            return;
-                        } else {
-                            System.out.println("Echoserver: echo " + line);
-                            output.println(line);
+            try {
+                Socket socket = this.serverSocket.accept();
+                new Thread(()-> {
+                    try {
+                        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintStream output = new PrintStream(socket.getOutputStream());
+
+                        while (true) {
+                            String line = input.readLine();
+                            if (line == null || line.equalsIgnoreCase("quit")) {
+                                socket.close();
+                                return;
+                            }
+                            else {
+                                output.println(line);
+                                output.flush();
+                            }
                         }
+                    } catch (IOException ex) {
+                        System.err.println("Echoserver: echo " + ex);
                     }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
+                }).start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
